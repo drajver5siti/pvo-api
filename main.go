1c2009be88d43e8df974be5b6a927465f4e57107
Ivan Pavlovski <ivan.pavlovskii@outlook.com>
2024-02-06 12:38:55 +0100

    Initial

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Result struct {
+	StatusCode int    `json:"statusCode"`
+	Time       int64  `json:"time"`
+	Data       string `json:"data"`
+}
+
+func parseLine(line string) string {
+	lines := strings.Split(line, " ")
+
+	result := 0
+
+	for _, num := range lines {
+		intNum, err := strconv.Atoi(num)
+
+		if err != nil {
+			continue
+		}
+
+		result += intNum
+	}
+
+	return strconv.Itoa(result)
+}
+
+func handleUpload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseMultipartForm(10 << 20)
+
+	if err != nil {
+		http.Error(w, "Unable to parse form", http.StatusBadRequest)
+		return
+	}
+
+	file, _, err := r.FormFile("file")
+
+	if err != nil {
+		http.Error(w, "Error while retreiving file", http.StatusInternalServerError)
+		io.WriteString(w, "0")
+		return
+	}
+
+	defer file.Close()
+
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+	scanner := bufio.NewScanner(file)
+
+	var result bytes.Buffer
+
+	for scanner.Scan() {
+		result.WriteString(parseLine(scanner.Text()))
+		result.WriteString(" ")
+	}
+
+	io.WriteString(w, result.String())
+	return
+}
+
+func main() {
+	http.HandleFunc("/upload", handleUpload)
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		panic(err)
+	}
+}
